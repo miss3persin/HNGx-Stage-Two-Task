36e1f1deda2321618d4d7029c81b261b1b909901
miss3persin <ige-bello.oluwasegunfunmi@lmu.edu.ng>
2023-09-12 16:26:09 +0100

    first try

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom';
 import Home from './Pages/Home';
+import Movie from './Pages/Movie';
+import SearchResults from './Pages/SearchResults';
 import App from './App.jsx'
 import './index.css'
 
@@ -9,10 +11,8 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<App/>}>
       <Route index={true} path='/' element={<Home/>}/>
-      {/* <Route path='/country/:countryName' element={<CountryDetails/>}/> */}
-      {/* Private Routes */}
-      {/* <Route path='' element={<PrivateRoute/>}>
-      </Route> */}
+      <Route path='/movies/:id' element={<Movie/>}/>
+      <Route path='/search/:query' element={<SearchResults/>}/>
     </Route>
   )
 );
